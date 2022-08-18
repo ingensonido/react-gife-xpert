e18be2eb5c9c295c15fb2c5e33e08572a5eb44a9
RZapata <rzapata@laradio.cl>
2022-08-18 13:49:01 -0400

    fix deploy netlify

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,7 @@ export const GifItem = ( { title, url, id } ) => {
     <div className="card">
         <img src={ url } alt={ title } />
         <p>{ title }</p>
-        <p>{ key }</p>
+        <p>{ id }</p>
     </div>
   )
 }
